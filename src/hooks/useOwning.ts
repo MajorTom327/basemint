fc71609417e556612604b8ce7df49dc32e194e2e
MajorTom327 <me@valentin-thomas.com>
2022-11-08 11:39:39 +0100

    Fix build

--- src/hooks/useOwning.ts
+++ src/hooks/useOwning.ts
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import eth from "../eth";
+import { StackingBuds } from "../types/Buds";
 
 
 
 export const useOwning = () => {
-  const [ownedToken, setOwned] = useState<Buds[]>([]);
+  const [ownedToken, setOwned] = useState<StackingBuds[]>([]);
 
   useEffect(() => {
     eth.walletOfOwner().then((tokens) => {
