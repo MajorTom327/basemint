ebbdc072faec13008736c5b3afd217507371f4d2
Valentin Thomas <me@valentin-thomas.com>
2022-12-06 21:20:14 +0100

    Update image

--- src/hooks/useBud.ts
+++ src/hooks/useBud.ts
@@ -1,6 +1,6 @@
 export const useBud = (id: number) => {
-  const budImage = `https://storage.googleapis.com/basemint-buds.appspot.com/${id}.gif`
-
+  const budImage = `https://storage.googleapis.com/basemint-buds.appspot.com/BaseMint%20Buds%20-%20Updated/${id}.gif`
+  // const budImage = `https://storage.googleapis.com/basemint-buds.appspot.com/${id}.gif`
   return {
     image: budImage
   };
